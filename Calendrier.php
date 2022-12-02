72cddc0059979601e3aa3f65295a90b84fc5a866
Léo Guiseppin <leo.guiseppin@gmail.com>
2022-12-02 08:30:51 +0100

    add files

---
+++ Calendrier.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./style/menu.css">
+    <title>Calendrier</title>
+</head>
+
+<body>
+    <ul id="menu">
+        <li class="menu-item"><a href="effectif.php">Effectif</a></li>
+        <li class="menu-item" style="border-bottom-style: solid; border-bottom-color: blue;"><a href="calendrier.php" style="color: blue;">Calendrier</a></li>
+    </ul>
+</body>
+
+</html>
---
+++ Effectif.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./style/menu.css">
+    <title>Effectif</title>
+</head>
+
+<body>
+    <ul id="menu">
+        <li class="menu-item" style="border-bottom-style: solid; border-bottom-color: blue;"><a href="effectif.php" style="color: blue;">Effectif</a></li>
+        <li class="menu-item"><a href="calendrier.php">Calendrier</a></li>
+    </ul>
+    
+</body>
+
+</html>
